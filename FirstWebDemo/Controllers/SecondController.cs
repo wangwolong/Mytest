682ac846892eedff3de1fdb4abe42c90e2315594
long <wangwolong@inexten.com>
2020-12-10 17:13:27 +0800

    第二个控制器

---
+++ FirstWebDemo/Controllers/SecondController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebDemo.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

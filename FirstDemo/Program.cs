6947d07361972a70481e6fff22ce2c0e5bd64ca5
wangjiyuan <wangjiyuan@inexten.com>
2020-12-11 10:00:43 +0800

    diwu

--- FirstDemo/Program.cs
+++ FirstDemo/Program.cs
@@ -10,6 +10,7 @@ namespace FirstDemo
             Console.WriteLine("2");
             Console.WriteLine("3");
             Console.WriteLine("4");
+            Console.WriteLine("5");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");

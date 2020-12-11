7edde8cd1f79a3085b1fe882a25ac1f68b3f8b1f
wangjiyuan <wangjiyuan@inexten.com>
2020-12-11 10:19:11 +0800

    更改  ‘提交’

--- FirstDemo/Program.cs
+++ FirstDemo/Program.cs
@@ -6,16 +6,8 @@ namespace FirstDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1");
-            Console.WriteLine("2");
-            Console.WriteLine("3");
-            Console.WriteLine("4");
-            Console.WriteLine("5");
-            Console.WriteLine("6");
-            Console.WriteLine("7");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("李四");
+          
         }
     }
 }

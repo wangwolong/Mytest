82988bb8f863adfa314fa45403967f79e5de5e93
wangjiyuan <wangjiyuan@inexten.com>
2020-12-11 09:59:24 +0800

    disi

--- FirstDemo/Program.cs
+++ FirstDemo/Program.cs
@@ -9,6 +9,7 @@ namespace FirstDemo
             Console.WriteLine("1");
             Console.WriteLine("2");
             Console.WriteLine("3");
+            Console.WriteLine("4");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");

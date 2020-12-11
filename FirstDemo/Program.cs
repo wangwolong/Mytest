a2f3a0c8d34c5f3d3a8894dc41d7e152b1aa56e6
wangjiyuan <wangjiyuan@inexten.com>
2020-12-11 09:51:08 +0800

    Revert "胡isAV发hi瓦尔"
    This reverts commit 0ee40f51bb273611d95d1d67703d2e070ec1d6c0.

--- FirstDemo/Program.cs
+++ FirstDemo/Program.cs
@@ -9,7 +9,6 @@ namespace FirstDemo
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
     }
 }

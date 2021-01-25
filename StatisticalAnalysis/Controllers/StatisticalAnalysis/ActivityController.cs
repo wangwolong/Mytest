e497080f976e17a8a14d2e06cfd11985e6b65a3e
wangwolong <wangwolong@inexten.com>
2021-01-25 14:22:28 +0800

    DownloadExcel和DownFile返回ActionResult可以进行页面下载

--- StatisticalAnalysis/Controllers/StatisticalAnalysis/ActivityController.cs
+++ StatisticalAnalysis/Controllers/StatisticalAnalysis/ActivityController.cs
@@ -11,11 +11,21 @@ namespace StatisticalAnalysis.Controllers.StatisticalAnalysis
 {
     public class ActivityController : BaseController
     {
+        /// <summary>
+        /// 调试swagger
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public JsonR Index()
         {
             return Common.ComEnum.Code.A_操作成功.JsonR();
         }
+        /// <summary>
+        /// 进行简单的查询
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="where"></param>
+        /// <returns></returns>
         [HttpPost]
         public JsonR Select(string tableName ,string where)
         {
@@ -23,23 +33,61 @@ namespace StatisticalAnalysis.Controllers.StatisticalAnalysis
             var sql = $"select * from {tableName } where {where}";
             return Common.ComEnum.Code.A_操作成功.JsonR(DB.Config.Conn_Wathet.Select<DB.activity>(sql));
         }
+        /// <summary>
+        /// 尝试加密
+        /// </summary>
+        /// <param name="encrypt"></param>
+        /// <returns></returns>
         [HttpGet]
         public JsonR DoEncrypt(string encrypt)
         {
             return Common.ComEnum.Code.A_操作成功.JsonR(Common.DoEncrypt.Encrypt(encrypt));
         }
+        /// <summary>
+        /// 尝试解密
+        /// </summary>
+        /// <param name="Decrypt"></param>
+        /// <returns></returns>
         [HttpGet]
         public JsonR DoDecrypt(string Decrypt)
         {
             return Common.ComEnum.Code.A_操作成功.JsonR(Common.DoEncrypt.Decrypt(Decrypt));
         }
+        /// <summary>
+        /// 尝试导出 把数据存到本地文件夹
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public JsonR Export()
         {
             var model = DB.Config.Conn_Wathet.Select<DB.activity>("select * from t_xw_activity");
-            var data = DoExcel.CreateReturnBytes(model);
-            System.IO.File.WriteAllBytes("Download\\Excel\\activity.xls", data);
+            var data = DoExcel.CreateReturnBytes(model);//转化为byte数组
+            System.IO.File.WriteAllBytes("Download\\Excel\\activity.xls", data);//写入本地文件夹
             return Common.ComEnum.Code.A_操作成功.JsonR("Download\\Excel\\activity.xls");
+        }
+        /// <summary>
+        /// 导出 把数据传到页面可以进行下载
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult DownFile()
+        {
+            var model = DB.Config.Conn_Wathet.Select<DB.activity>("select * from t_xw_activity");
+            var data = DoExcel.CreateReturnBytes(model);
+            return File(data, "application/octet-stream", "activity.xls");
+        }
+        /// <summary>
+        /// 导出 根据文件名称导出本地特定文件
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult DownloadExcel(string fileName)
+        {
+            fileName += ".xls";
+            var url = "Download\\Excel\\"+ fileName;
+            var stream = new System.Net.WebClient().OpenRead(url);
+            return File(stream, "application/octet-stream", fileName);
         }
     }
 }

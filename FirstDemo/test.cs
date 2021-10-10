54dc7d4631ece47f0b32b650bc7c5d29acce1924
wangwolong <wangwolong@inexten.com>
2021-10-10 23:15:23 +0800

    test提交

---
+++ FirstDemo/test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstDemo
+{
+    public class test
+    {
+        public void stater()
+        {
+            Console.WriteLine("成功");
+        }
+    }
+}
